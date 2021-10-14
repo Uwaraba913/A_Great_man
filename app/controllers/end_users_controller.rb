235269f8c4fa7e6a162e89d043437b821768a32c
Uwaraba913 <metalcoola5963@gmail.com>
2021-10-14 12:11:38 +0000

    ヘッダー調節他

--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -30,11 +30,13 @@ class EndUsersController < ApplicationController
   def following
     @end_user = EndUser.find(params[:id])
     @end_users = @end_user.followings
+    @quotes = @end_user.quote.limit(3).order(created_at: :desc)
   end
 
   def followers
     @end_user = EndUser.find_by(id: params[:id])
     @end_users = @end_user.followers
+    @quotes = @end_user.quote.limit(3).order(created_at: :desc)
   end
 
   private
